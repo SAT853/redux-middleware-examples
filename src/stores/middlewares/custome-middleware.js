4554de6bb8a2e7184f7922acc08b141430ff9e31
Sathishkumar Raja <sathishkumar.raja@one.verizon.com>
2021-03-12 11:21:53 +0530

    updated custom middleware

---
+++ src/stores/middlewares/custome-middleware.js
@@ -0,0 +1,28 @@
+const forbiddenWords = ["spam", "money"];
+
+// Custome Middleware
+export const forbiddenWordsMiddleware = ({ dispatch }) => (next) => (action) => {
+  // Do your stuff
+  if (action.type === "articlesReducers/addArticles") {
+    const isWordFound = forbiddenWords.filter((word) => action.payload.title.includes(word));
+    if (isWordFound.length) {
+      alert("Found Bad Word!");
+      return dispatch({ type: "FOUND_BAD_WORD" });
+    }
+  }
+  return next(action);
+};
+
+// export const fetchArticlesMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+//   if (action.type === FETCH_REQUESTED) {
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then((response) => response.json())
+//       .then((json) => {
+//         return dispatch({ type: "articlesReducers/dataLoaded", payload: json });
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }
+//   return next(action);
+// };
