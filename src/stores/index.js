35ba58087fe0387792a42a9168ab6a7fad9e3202
Sathishkumar Raja <sathishkumar.raja@one.verizon.com>
2021-03-12 11:21:03 +0530

    updated custome middleware path and added default middleware

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,14 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 
 import { articlesReducers } from "./articleReducers";
 import { countReducers } from "./countReducers";
-import { forbiddenWordsMiddleware } from "./middlewares";
+import { forbiddenWordsMiddleware } from "./middlewares/custome-middleware";
 import apiSaga from "./middlewares/sagas/api-saga";
 
 const initialiseSagaMiddleware = createSagaMiddleware();
 
-const middleware = [forbiddenWordsMiddleware, initialiseSagaMiddleware];
+const middleware = [forbiddenWordsMiddleware, initialiseSagaMiddleware, ...getDefaultMiddleware()];
 
 const store = configureStore({ reducer: { articles: articlesReducers, count: countReducers }, middleware });
 
