527de40c12944bbbb030c9700e34e32c8b66324d
Sathishkumar Raja <sathishkumar.raja@one.verizon.com>
2021-03-16 22:22:50 +0530

    added few more functions to api

--- src/stores/middlewares/sagas/api.js
+++ src/stores/middlewares/sagas/api.js
@@ -1,3 +1,22 @@
 export const getData = async () => {
   return fetch("https://jsonplaceholder.typicode.com/posts").then((response) => response.json());
 };
+
+export const authorize = () => {
+  // Do something
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("_" + Math.random().toString(36).substr(2, 9));
+    }, 2000);
+  });
+};
+
+export const clearItem = () => {
+  // Clear Item
+  console.log("item cleared...");
+};
+
+export const storeItem = () => {
+  // Store item
+  console.log("item stored...");
+};
