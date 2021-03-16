8b07bec0c5a453cdebf6d05abf3651bb111f541e
Sathishkumar Raja <sathishkumar.raja@one.verizon.com>
2021-03-16 22:22:20 +0530

    added login, logout button and comment rest of all

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from "react";
-import Articles from "./components/Articles";
-import Post from "./components/Post";
-import Button from "./components/Button";
-import Count from "./components/Count";
+import { connect } from "react-redux";
+import Button from "./components/Common/Button";
+// import Articles from "./components/Articles";
+// import Post from "./components/Post";
+// import Button from "./components/Button";
+// import Count from "./components/Count";
 
-function App() {
+function App({ login, logout }) {
   return (
     <div className="App">
       <h1> Redux-Saga Examples </h1>
-      <Count />
+      {/* <Count />
       <Button />
       <Articles />
       <div>
         <h2>API posts</h2>
         <Post />
-      </div>
+      </div> */}
+      <Button onClick={() => login()}>Login</Button>
+      <Button onClick={() => logout()}>Logout</Button>
     </div>
   );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: () => dispatch({ type: "LOGIN_REQUEST" }),
+    logout: () => dispatch({ type: "LOGOUT" }),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
